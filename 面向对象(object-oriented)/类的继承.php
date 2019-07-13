7330d76359c13d656be4f753e85483767852729b
OcaenEyes <1450136519@qq.com>
2019-07-13 18:30:42 +0800

    Update 类的继承.php

--- 面向对象(object-oriented)/类的继承.php
+++ 面向对象(object-oriented)/类的继承.php
@@ -5,3 +5,18 @@
  * Date: 2019/7/13
  * Time: 16:26
  */
+class Dad {
+    public function Tbook() {
+        echo "book";
+    }
+}
+
+class Boy extends Dad {
+    public function run(){
+        $this ->Tbook();
+    }
+
+}
+
+$boy = new Boy();
+$boy ->run();
